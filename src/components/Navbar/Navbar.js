a68b6950a34aeaa4096b163fbcaf8e334b6cfe02
Amit Kumar Mahto <amit.mahto3097@gmail.com>
2023-12-19 13:04:50 +0530

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import "../Navbar/Navbar.css";
+import { Link } from "react-router-dom";
+
+//get button name and path for every component
+const Navbar = (props) => {
+  return (
+    <div className="navbar">
+      <h2 onClick={() => props.handleClick(false)}>
+        <span className="brand-first-half">MEMENTO</span>
+        <span className="brand-last-half">DIRECTORY</span>
+      </h2>
+      <Link to={props.path}>
+        <button>{props.page}</button>
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;
